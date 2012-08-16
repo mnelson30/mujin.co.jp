5a3411496bf0562985ccc21159edbdaeb71bbea7
Rosen Diankov <rosen.diankov@mujin.co.jp>
2012-08-17 03:05:34 +0900

    fixed updateweb

--- mujinwww/updateweb.bash
+++ mujinwww/updateweb.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd ~/www/mujinwww
+git reset --hard HEAD # remove any local changes
+git pull
 python manage.py syncdb
 django-admin.py compilemessages --locale=ja_JP
 ln -s -f `pwd`/mujinwww/static ~/static
