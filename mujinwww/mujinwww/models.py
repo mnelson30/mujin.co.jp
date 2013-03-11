5a657c453856e7cc56e73908ea89c40e4511800f
Lucas Doyle <lukedoyle@mujin.co.jp>
2013-03-11 10:44:25 +0900

    improved models and admin

--- mujinwww/mujinwww/models.py
+++ mujinwww/mujinwww/models.py
@@ -13,20 +13,34 @@ LANGUAGES = (
     (JAPANESE, _('Japanese'))
 )
 
-class NewsEntry(models.Model):
-    title = models.CharField(max_length=230)
+class TranslatedNewsEntry(models.Model):
     language = models.CharField(max_length=230, default=ENGLISH, choices=LANGUAGES)
-    pub_date = models.DateTimeField(default=datetime.datetime.now)
-    content = models.TextField(help_text='This can have HTML in it')
+    title = models.CharField(max_length=230)
+    content = models.TextField(help_text='HTML supported')
 
     def __unicode__(self):
-        return self.title + ' ' + self.language
+        return self.title
+
+class NewsEntry(models.Model):
+    pub_date = models.DateTimeField(default=datetime.datetime.now)
+    en = models.ForeignKey(TranslatedNewsEntry, unique=True, related_name=ENGLISH)
+    ja = models.ForeignKey(TranslatedNewsEntry, unique=True, related_name=JAPANESE)
+
+    def __unicode__(self):
+        if hasattr(self.en, 'title'):
+            return '%s (%s)'%(self.en.title, self.pub_date)
+        else:
+            return self.pub_date
+
+class TranslatedUserProfile(models.Model):
+    language = models.CharField(max_length=230, default=ENGLISH, choices=LANGUAGES)
+    bio = models.TextField(help_text='HTML supported')
 
 # these are employees
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-
-    language = models.CharField(max_length=230, default=ENGLISH, choices=LANGUAGES)
+    en = models.ForeignKey(TranslatedUserProfile, unique=True, related_name=ENGLISH)
+    ja = models.ForeignKey(TranslatedUserProfile, unique=True, related_name=JAPANESE)
 
     def _photo_path(instance, orig_path):
         filename = (unicode(instance) + '_' + orig_path).replace(' ', '_')
@@ -35,10 +49,9 @@ class UserProfile(models.Model):
         return str(path)
 
     photo = models.ImageField(upload_to=_photo_path)
-    bio = models.TextField(help_text='This can have HTML in it')
 
     def __unicode__(self):
-        return self.user.username + ' ' + self.language
+        return self.user.username
 
 #~ enables us to reference the users profile and have it created if it does not exist
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
