c95624de1c49e1541c4f632647753dc238754182
Lucas Doyle <lukedoyle@mujin.co.jp>
2013-11-15 09:05:04 +0900

    all pages for the most part done

--- js/main.js
+++ js/main.js
@@ -6,13 +6,20 @@ $('.carousel').carousel({
 // tooltips for social links
 $('.tooltip-social').tooltip({
   selector: "a[data-toggle=tooltip]"
-})
-
-$('body').scrollspy({
-  target: '#nav-sidebar',
-  offset: 80
 });
 
-$(window).on('load', function () {
-  $body.scrollspy('refresh')
-})
+
+$('#form_submit').click(function() {
+  var body = '';
+  $('#contactform [data-human-name]').each(function() {
+    var $this = $(this);
+
+    body += '[' + $this.data('humanName') + ']\n';
+    body += $this.val() + '\n';
+    body += '\n';
+  });
+  var subject = 'New inquiry from ' + $('#first_name').val() + ' '  + $('#last_name').val() 
+  
+  var address = 'mailto:' + encodeURI('info@mujin.co.jp?subject=' + subject + '&body=' + body);
+  window.location = window.location.href = address;
+});
